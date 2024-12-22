86fe8330055e569be9ccf7ac35a794867f579956
Maximilian Sander <maximilian.sander1@tu-dresden.de>
2024-12-22 23:37:15 +0100

    Fix perplexity run script

--- run_perplexity_eval.sh
+++ run_perplexity_eval.sh
@@ -11,7 +11,7 @@ PREFIX="/data/horse/ws/s0872522-llm-zfp/llama.cpp/Meta-Llama-3-8B/Meta-Llama-3-8
 DIM="4"
 export NCPUS=8
 
-./_build/bin/llama-perplexity -ngl 300 --ctx-size 4096 -s 1 -t ${NCPUS} --perplexity -m ${PREFIX}-F16.gguf --file ./Meta-Llama-3-8B/wiki.train.raw --chunks 10 2>&1 | tee ppl.F16
+./_build/bin/llama-perplexity -ngl 300 --ctx-size 4096 -s 1 -t ${NCPUS} --perplexity -m ${PREFIX}-F16.gguf --file ./Meta-Llama-3-8B/wiki.train.raw 2>&1 | tee ppl.F16
 echo "F16 -- $(grep 'Final estimate: PPL =' ppl.F16)" >> "$OUTPUT_SUMMARY"
 for rate in 3.0 3.5 4.0 4.5 5.0 6.0 8.0 ; do
     echo $rate
@@ -19,7 +19,7 @@ for rate in 3.0 3.5 4.0 4.5 5.0 6.0 8.0 ; do
     
     OUTPUT_NAME="from_ZFPRATE_${ZFP_RATE}_dim_${DIM}"
     
-    ./_build/bin/llama-perplexity -ngl 300 --ctx-size 4096 -s 1 -t ${NCPUS} --perplexity -m ${PREFIX}-F16_${OUTPUT_NAME}.gguf --file ./Meta-Llama-3-8B/wiki.train.raw | tee ppl.${OUTPUT_NAME}
+    ./_build/bin/llama-perplexity -ngl 300 --ctx-size 4096 -s 1 -t ${NCPUS} --perplexity -m ${PREFIX}-F16_${OUTPUT_NAME}.gguf --file ./Meta-Llama-3-8B/wiki.train.raw 2>&1 | tee ppl.${OUTPUT_NAME}
     echo "${OUTPUT_NAME} -- $(grep 'Final estimate: PPL =' ppl.${OUTPUT_NAME})" >> $OUTPUT_SUMMARY
 done
 
@@ -29,6 +29,6 @@ for tol in 0.01 0.1 0.12 0.13 ; do
     export ZFP_TOL=$tol
     OUTPUT_NAME="from_ZFPTOL_${ZFP_TOL}_dim_${DIM}"
 
-    ./_build/bin/llama-perplexity -ngl 300 --ctx-size 4096 -s 1 -t ${NCPUS} --perplexity -m ${PREFIX}-F16_${OUTPUT_NAME}.gguf --file ./Meta-Llama-3-8B/wiki.train.raw | tee ppl.${OUTPUT_NAME}
+    ./_build/bin/llama-perplexity -ngl 300 --ctx-size 4096 -s 1 -t ${NCPUS} --perplexity -m ${PREFIX}-F16_${OUTPUT_NAME}.gguf --file ./Meta-Llama-3-8B/wiki.train.raw 2>&1 | tee ppl.${OUTPUT_NAME}
     echo "${OUTPUT_NAME} -- $(grep 'Final estimate: PPL =' ppl.${OUTPUT_NAME})" >> $OUTPUT_SUMMARY
 done
