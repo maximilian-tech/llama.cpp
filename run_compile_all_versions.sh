31b76891c63d27fa3f67b1dc694a28e9a973688e
Maximilian Sander <maximilian.sander1@tu-dresden.de>
2025-02-27 20:14:23 +0100

    Redo compile all convenience script

--- run_compile_all_versions.sh
+++ run_compile_all_versions.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 8
+#SBATCH --mem=16G
+#SBATCH -A p_lv_scc25
+#SBATCH --time=03:00:00
+#SBATCH --hint=multithread
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
-set -euo pipefail
+module purge
+
+source $SCRIPT_DIR/../source_env_llvm.rc
 
 echo "<--> Starting Compile Default"
 
